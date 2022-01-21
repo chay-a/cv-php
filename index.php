9b8793103e0e6854bf93157233c22d1f09aab042
chay-a <chaya.07@protonmail.com>
2022-01-21 19:25:41 +0100

    etape 3.1 front controller

--- index.php
+++ index.php
@@ -6,5 +6,13 @@ if (isset($_GET['page'])){
         include 'pages/contact.php';
     }
 } else {
-       include 'pages/hobby.php';
+       include 'pages/accueil.php';
 }
+
+//if ($_SERVER['REQUEST_URI'] == 'hobby') {
+//    include 'pages/hobby.php';
+//} elseif ($_SERVER['REQUEST_URI'] == 'contact') {
+//    include 'pages/contact.php';
+//} else {
+//    include 'pages/accueil.php';
+//}
