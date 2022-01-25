7bd42010e82b817e960e7ab8f7b897b85f3d3c49
chay-a <chaya.07@protonmail.com>
2022-01-25 21:41:37 +0100

    fix: inputs sanitized and validate

--- pages/contact.php
+++ pages/contact.php
@@ -1,33 +1,86 @@
 <?php
 $is_FormSubmit = filter_has_var(INPUT_POST, 'submit');
 $is_Sent = false;
+$msgError = array(
+    'civilite_selection' => '',
+    'name' => '',
+    'firstname' => '',
+    'email' => '',
+    'raison_contact' => '',
+    'message' => '',
+);
+$InputsValidate;
 if ($is_FormSubmit) {
-    $is_civilite = filter_input(INPUT_POST, 'civilite_selection', FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "monsieur|madame|autre")));
-    $is_nom = filter_input(INPUT_POST, 'name', FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/[-'’\p{L}\p{M}\p{N}]+/um")));
-    $is_prenom = filter_input(INPUT_POST, 'firstname', FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/[-'’\p{L}\p{M}\p{N}]+/um")));
-    $is_email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-    $is_raisonContact = filter_input(INPUT_POST, 'raison_contact', FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "proposition d\'emploi|demande d\'information|prestations")));
-    $message = filter_input(INPUT_POST, 'message');
-    $is_MessageGreaterThan5 = mb_strlen($message, 'UTF-8') > 5;
-    $civilite = filter_input(INPUT_POST, 'civilite_selection');
-    $nom = filter_input(INPUT_POST, 'name');
-    $prenom = filter_input(INPUT_POST, 'firstname');
-    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $raisonContact = filter_input(INPUT_POST, 'raison_contact');
-    if ($is_civilite && $is_nom && $is_prenom && $is_email && $is_raisonContact && $is_MessageGreaterThan5) {
-        $entreCivilite = 'Civilité : ' . $civilite . PHP_EOL;
-        $entreNom = 'Nom : ' . $nom . PHP_EOL;
-        $entrePrenom = 'Prénom : ' . $prenom . PHP_EOL;
-        $entreEmail = 'email : ' . $is_email . PHP_EOL;
-        $entreRaisonContact = 'Raison contact : ' . $raisonContact . PHP_EOL;
-        $entreMessage = 'Message : ' . $message . PHP_EOL;
+    $optionsSanitize = array(
+        'civilite_selection' => FILTER_SANITIZE_STRING,
+        'name' => FILTER_SANITIZE_STRING,
+        'firstname' =>FILTER_SANITIZE_STRING,
+        'email' => FILTER_SANITIZE_EMAIL,
+        'raison_contact' => array(
+                'filter' => FILTER_SANITIZE_STRING,
+                'flags' => FILTER_FLAG_NO_ENCODE_QUOTES,
+        ),
+        'message' => FILTER_SANITIZE_STRING,
+    );
+    $inputsSanitize = filter_input_array(INPUT_POST, $optionsSanitize);
+    $optionsValidate = array(
+        'civilite_selection' => array(
+                'filter' => FILTER_CALLBACK,
+                'options' => 'filter_validate_civilite',
+        ),
+        'name' => array(
+                'filter' => FILTER_VALIDATE_REGEXP,
+                'options' => array(
+                'regexp' => "/[-'’\p{L}\p{M}\p{N}]+/um",
+            ),
+        ),
+        'firstname' =>array(
+                'filter' => FILTER_VALIDATE_REGEXP,
+                'options' => array(
+                'regexp' => "/[-'’\p{L}\p{M}\p{N}]+/um",
+            ),
+        ),
+        'email' => FILTER_VALIDATE_EMAIL,
+        'raison_contact' => array(
+                'filter' => FILTER_CALLBACK,
+                'options' => 'filter_validate_raison_contact',
+        ),
+        'message' => array(
+                'filter' => FILTER_CALLBACK,
+                'options' => 'filter_validate_message',
+        ),
+    );
+    $InputsValidate = filter_var_array($inputsSanitize, $optionsValidate);
+    $msgErrorFalse = array(
+            'civilite_selection' => ' choisir une civilité',
+            'name' => 'entrer un nom valide (seulement lettres et tiret)',
+            'firstname' => 'entrer un prénom valide (seulement lettres et tiret)',
+            'email' => 'entrer un adresse email valide',
+            'raison_contact' => 'choisir une raison de contact',
+            'message' => 'entrer un message de plus de 5 lettres',
+    );
+    $nbError = 0;
+    foreach ($optionsValidate as $key => $value) {
+        if (empty($InputsValidate[$key])) {
+            $msgError[$key] = 'Veuiller remplir le champ ' . $key;
+            $nbError++;
+        } elseif ($InputsValidate[$key] === false) {
+            $msgError[$key] = 'Veuillez '.$msgErrorFalse[$key];
+            $nbError++;
+        }
+    }
+
+    if ($nbError === 0) {
+        $fileContent = '';
+        foreach ($InputsValidate as $key => $value) {
+            $fileContent = $fileContent . $key . ' : ' . $value . PHP_EOL;
+        }
         $today = date("Y-m-d-H-i-s");
         $file = 'contact/contact_' . $today . '.txt';
-        file_put_contents($file, $entreCivilite . $entreNom . $entrePrenom . $entreEmail . $entreRaisonContact . $entreMessage);
+        file_put_contents($file, $fileContent);
         $is_Sent = true;
     }
 }
-
 ?>
 <h1>Contact</h1>
 <!--Lien pour faire un mail-->
@@ -41,81 +94,75 @@ if ($is_FormSubmit) {
             </div>
             <select name="civilite_selection">
                 <option value="">Choisir une civilité</option>
-                <option value="monsieur" <?php if (isset($is_civilite) && $is_civilite && $civilite == 'monsieur' && !$is_Sent) : ?> selected="selected" <?php endif; ?>>
+                <option value="monsieur" <?php if (isset($InputsValidate) && $InputsValidate['civilite_selection'] == "monsieur" && !$is_Sent) : ?> selected="selected" <?php endif; ?>>
                     Monsieur
                 </option>
-                <option value="madame" <?php if (isset($is_civilite) && $is_civilite && $civilite == 'madame' && !$is_Sent) : ?> selected="selected" <?php endif; ?>>
+                <option value="madame" <?php if (isset($InputsValidate) && $InputsValidate['civilite_selection'] == "madame" && !$is_Sent) : ?> selected="selected" <?php endif; ?>>
                     Madame
                 </option>
-                <option value="autre" <?php if (isset($is_civilite) && $is_civilite && $civilite == 'autre' && !$is_Sent) : ?> selected="selected" <?php endif; ?>>
+                <option value="autre" <?php if (isset($InputsValidate) && $InputsValidate['civilite_selection'] == "autre" && !$is_Sent) : ?> selected="selected" <?php endif; ?>>
                     Autre
                 </option>
             </select>
-            <?php if (isset($is_civilite) && !$is_civilite && $is_FormSubmit) : ?>
-                <p class="error">Veuillez choisir une civilité</p>
+            <?php if (empty($is_InputsValidate['civilite_selection']) || $is_InputsValidate['civilite_selection'] === false) :?>
+            <p class="error"><?= $msgError['civilite_selection'] ?></p>
             <?php endif; ?>
         </div>
         <div class="form_name input">
             <div class="label_nom label">
-
                 <label for="name">Votre nom</label>
             </div>
             <input type="text" placeholder="Votre nom" name="name"
-                   class="input_text" <?php if (isset($is_nom) && $is_nom && !$is_Sent) : ?> value="<?= $nom ?>" <?php endif; ?>>
-            <?php if ($is_FormSubmit) : if (isset($is_nom) && !$is_nom) :?>
-                <p class="error">Veuillez entrer un nom correcte (sans chiffres...)</p>
-            <?php elseif(empty($nom)) :?>
-                <p class="error">Veuillez entrer un nom</p>
-            <?php endif; endif; ?>
+                   class="input_text" <?php if (isset($InputsValidate) && $InputsValidate['name'] && !$is_Sent) : ?> value="<?= $InputsValidate['name'] ?>" <?php endif; ?>>
+            <?php if (empty($is_InputsValidate['name']) || $is_InputsValidate['name'] === false) :?>
+                <p class="error"><?= $msgError['name'] ?></p>
+            <?php endif; ?>
         </div>
         <div class="form_firstname input">
             <div class="label_prenom label">
-
                 <label for="firstname">Votre prénom</label>
             </div>
             <input type="text" placeholder="Votre prénom" name="firstname"
-                   class="input_text" <?php if (isset($is_prenom) && $is_prenom && !$is_Sent) : ?> value="<?= $prenom ?>" <?php endif; ?>>
-            <?php if ($is_FormSubmit) : if (isset($is_prenom) && !$is_prenom) :?>
-                <p class="error">Veuillez entrer un prénom correcte (sans chiffres...)</p>
-            <?php elseif(empty($prenom)) :?>
-                <p class="error">Veuillez entrer un prénom</p>
-            <?php endif; endif;?>
+                   class="input_text" <?php if (isset($InputsValidate) && $InputsValidate['firstname'] && !$is_Sent) : ?> value="<?= $InputsValidate['firstname'] ?>" <?php endif; ?>>
+            <?php if (empty($is_InputsValidate['firstname']) || $is_InputsValidate['firstname'] === false) :?>
+                <p class="error"><?= $msgError['firstname'] ?></p>
+            <?php endif; ?>
         </div>
         <div class="form_email input">
             <div class="label_email label">
                 <label for="email">Votre email</label>
             </div>
             <input type="email" placeholder="votre email" name="email"
-                   class="input_text" <?php if (isset($is_email) && $is_email && !$is_Sent) : ?> value="<?= $email ?>" <?php endif; ?>>
-            <?php if (empty($is_email) && $is_FormSubmit) : ?>
-                <p class="error">Veuillez entrer votre email</p>
+                   class="input_text" <?php if (isset($InputsValidate) && $InputsValidate['email'] && !$is_Sent) : ?> value="<?= $InputsValidate['email'] ?>" <?php endif; ?>>
+            <?php if (empty($is_InputsValidate['email']) || $is_InputsValidate['email'] === false) :?>
+                <p class="error"><?= $msgError['email'] ?></p>
             <?php endif; ?>
         </div>
         <div class="form_raison_contact">
             <p>Pour quelles raison me contactez-vous?</p>
             <div>
                 <input type="radio" name="raison_contact"
-                       value="proposition d'emploi" <?php if (isset($raisonContact) && $raisonContact == "proposition d'emploi" && !$is_Sent) {
+                       value="proposition d'emploi" <?php if (isset($InputsValidate) && $InputsValidate['raison_contact'] == "proposition d'emploi" && !$is_Sent) {
                     echo 'checked';
                 } ?>>
                 <label for="proposition d'emploi">Proposition d'emploi</label>
             </div>
             <div>
                 <input type="radio" name="raison_contact"
-                       value="demande d'information" <?php if (isset($raisonContact) && $raisonContact == "demande d'information" && !$is_Sent) {
+                       value="demande d'information" <?php if (isset($InputsValidate) && $InputsValidate['raison_contact'] == "demande d'information" && !$is_Sent) {
                     echo 'checked';
                 } ?>>
                 <label for="demande d'information">Demande d'information</label>
             </div>
             <div>
                 <input type="radio" name="raison_contact"
-                       value="prestations" <?php if (isset($raisonContact) && $raisonContact == "prestations" && !$is_Sent) {
+                       value="prestations" <?php if (isset($InputsValidate) && $InputsValidate['raison_contact'] == "prestations" && !$is_Sent) {
                     echo 'checked';
                 } ?>>
                 <label for="prestations">Prestations</label>
             </div>
-            <?php if (empty($raisonContact) && $is_FormSubmit) : ?>
-                <p class="error">Veuillez choisir une raison de contact</p>
+            <?php if (empty($is_InputsValidate['raison_contact']) || $is_InputsValidate['raison_contact'] === false) :?>
+                <p class="error"><?= $msgError['raison_contact'] ?></p>
             <?php endif; ?>
         </div>
         <div class="form_texte input">
@@ -123,11 +170,37 @@ if ($is_FormSubmit) {
                 <label for="message">Votre message</label>
             </div>
             <textarea name="message" id="message" cols="30"
-                      rows="10"><?php if (!empty($message) && !$is_Sent) : echo $message; endif; ?></textarea>
-            <?php if ((empty($message) || !$is_MessageGreaterThan5) && $is_FormSubmit) : ?>
-                <p class="error">Veuillez entrer un message de plus de 5 charactères</p>
+                      rows="10"><?php if (isset($InputsValidate) && $InputsValidate['message'] && !$is_Sent) : echo $InputsValidate['message']; endif; ?></textarea>
+            <?php if (empty($is_InputsValidate['message']) || $is_InputsValidate['message'] === false) :?>
+                <p class="error"><?= $msgError['message'] ?></p>
             <?php endif; ?>
         </div>
         <input type="submit" value="Envoyer" class="envoyer" name="submit">
     </div>
 </form>
+
+<?php
+
+function filter_validate_civilite($civiliteAtester) {
+    if ($civiliteAtester === 'monsieur' || $civiliteAtester === 'madame' || $civiliteAtester === 'autre') {
+        return $civiliteAtester;
+    } else {
+        return false;
+    }
+}
+
+function filter_validate_raison_contact($raisonContactATester) {
+    if ($raisonContactATester == "proposition d'emploi" || $raisonContactATester == "demande d'information" || $raisonContactATester == "prestations") {
+        return $raisonContactATester;
+    } else {
+        return false;
+    }
+}
+
+function filter_validate_message($messageATester) {
+    if (mb_strlen($messageATester) > 5) {
+        return $messageATester;
+    } else {
+        return false;
+    }
+}
