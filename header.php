1cbb8496dec31224486332b8b06b1c2c773626ea
Chay Auriane <chaya.07@protonmail.com>
2022-01-27 15:26:51 +0100

    refacto : meta données utilisation du tableau routes

--- header.php
+++ header.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?php echo $metaDescription; ?>"/>
+    <meta name="description" content="<?php echo $routes[$queryPage][2]; ?>"/>
     <link rel="stylesheet" href="style/style.css">
     <?php if ($queryPage == 'hobby') : ?>
         <link rel="stylesheet" href="style/hobby.css">
     <?php elseif ($queryPage == 'contact') : ?>
         <link rel="stylesheet" href="style/contact.css">
     <?php endif; ?>
-    <title><?php echo $metaTitre . ' | Auriane Chay'; ?></title>
+    <title><?php echo $routes[$queryPage][1] . ' | Auriane Chay'; ?></title>
 </head>
 
 <body>
@@ -25,7 +25,7 @@
     <!--Navigation -->
     <nav>
         <?php foreach($routes as $key => $value) :?>
-            <a href="index.php<?php if ($key !== NULL) { echo "?page=$key";} ?>" <?php if ($queryPage == $key) : ?> class="active" <?php endif; ?>><?= $value[1]?></a>
+            <a href="index.php<?php if ($key != NULL) { echo "?page=$key";} ?>" <?php if ($queryPage == $key) : ?> class="active" <?php endif; ?>><?= $value[1]?></a>
         <?php endforeach;?>
     </nav>
 </header>
